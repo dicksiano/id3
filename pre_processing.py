90f2e8cf00f1ce51e8c36fde9e2384ddf3063d55
dicksiano <dicksianomelo@gmail.com>
2018-09-27 06:11:09 -0300

    Initial commit, pre-processing input data

---
+++ pre_processing.py
@@ -0,0 +1,26 @@
+def dataset(file):
+    lines = open(file, encoding="ISO-8859-1").readlines()
+    return [line.split("::") for line in lines]
+
+def pre_process_movies():
+    dict = {}
+    for line in dataset('movies.dat'):
+        dict[ int(line[0]) ] = line[2][:-1]
+    return dict
+
+def pre_process_users():
+    dict = {}
+    for line in dataset('users.dat'):
+        dict[ int(line[0]) ] = [line[1], line[2], line[3]]
+    return dict
+
+def pre_process_ratings():
+    movies, users, res = pre_process_movies(), pre_process_users(), []
+
+    for line in dataset('ratings.dat'):
+        tmp = users[ int(line[0]) ]
+        res.append([tmp[0], tmp[1], tmp[2], movies[ int(line[1]) ], line[2]])
+
+    return res
+
+print(pre_process_ratings())
