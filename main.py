b9347814dee1af33c0333aebd5030bab7f5e5fb7
dicksiano <dicksianomelo@gmail.com>
2018-09-28 06:54:04 -0300

    Updates os metrics

--- main.py
+++ main.py
@@ -3,17 +3,36 @@ from id3 import *
 
 input = pre_process_ratings()
 
-data = input[::100]
+data = input[::1000]
 test = input
 
 tree = id3(data)
 #printtree(tree)
 
 correct = 0
+fp = 0
+fn = 0
+tp = 0
+tn = 0
 for t in test:
     a = predict(t[:-1],tree)
     b = t[-1]
-    correct += int(max(a, key=a.get) == b)
 
-print(float(correct)/len(test))
+    neg = pos = 0
+    if 0 in a:
+        neg = a[0]
+    if 1 in a:
+        posit = a[1]
+
+    result = 0
+    if posit >= neg: result = 1
+
+    if result == 1:
+        if result == b: tp += 1
+        else: fp += 1
+    else:
+        if result == b: tn += 1
+        else: fn += 1
+
+print(float(tp)/len(test), float(tn)/len(test), float(fp)/len(test), float(fn)/len(test))
     
